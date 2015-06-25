49af1c87a51bf421a96a5402e4ac6f654d980851
satyajitdey02 <satyajit02@gmail.com>
2015-06-25 16:20:11 +0600

    BB: Snowflakes- An unique Long ID generator Added

---
+++ blue-bits-snowflakes/src/main/java/org/bluebits/snowflakes/GetHardwareIdFailedException.java
@@ -0,0 +1,17 @@
+package org.bluebits.snowflakes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: predictor
+ * Date: 25.01.13
+ * Time: 11:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class GetHardwareIdFailedException extends Exception {
+    GetHardwareIdFailedException(String reason){
+        super(reason);
+    }
+    GetHardwareIdFailedException(Exception e){
+        super(e);
+    }
+}
